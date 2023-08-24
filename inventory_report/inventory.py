d40aa3ede4d2f63e5749335b8b70a9e3bcc7b8e3
gus-aguiar <gustavopma90@gmail.com>
2023-08-24 18:00:14 -0300

    req 5

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,16 @@
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: list[Product] | None = None) -> None:
+        if data is None:
+            self._data = []
+        else:
+            self._data = data
+
+    def add_data(self, data: list[Product]) -> None:
+        self.data.extend(data)
+
+    @property
+    def data(self) -> list[Product]:
+        return self._data
