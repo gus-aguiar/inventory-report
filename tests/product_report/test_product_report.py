2822e716b82a2d86b6851cec5c2a7cf4a5fa7427
gus-aguiar <gustavopma90@gmail.com>
2023-08-24 14:54:56 -0300

    req 2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,22 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product = Product(
+        id="1",
+        product_name="product name",
+        company_name="company name",
+        manufacturing_date="2022-01-01",
+        expiration_date="2023-01-01",
+        serial_number="123456",
+        storage_instructions="Instructions",
+    )
+    assert product.__str__() == (
+        f"The product {product.id} - {product.product_name} "
+        f"with serial number {product.serial_number} "
+        f"manufactured on {product.manufacturing_date} "
+        f"by the company {product.company_name} "
+        f"valid until {product.expiration_date} "
+        "must be stored according to the following instructions: "
+        f"{product.storage_instructions}."
+    )
